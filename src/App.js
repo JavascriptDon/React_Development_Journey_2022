e6fba7bf3429f1c6e9352e870964418dc47cf533
hr21don <h_R@helitharupasinghe.com>
2022-08-06 12:30:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
